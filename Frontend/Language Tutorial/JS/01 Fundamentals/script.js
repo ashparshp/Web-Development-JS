39be673bdc2e2f19c46f97147449b5074e6c91b3
Ashparshp <ashparsh.connects@gmail.com>
2025-10-07 07:23:54 +0530

    feat: add if-else alternatives for day-based actions in addition to switch statement

--- Frontend/Language Tutorial/JS/01 Fundamentals/script.js
+++ Frontend/Language Tutorial/JS/01 Fundamentals/script.js
@@ -87,7 +87,7 @@ console.log(!largeNumber); // false (NOT operator)
 const day = "monday";
 
 switch (day) {
-  case "monday":
+  case "monday": // day === "monday" this is a strict comparison
     console.log("Plan course structure");
     console.log("Go to coding meetup");
     break;
@@ -108,3 +108,20 @@ switch (day) {
   default:
     console.log("Not a valid day!");
 }
+
+// Alternate ways using if-else
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+
